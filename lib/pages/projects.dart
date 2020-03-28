2fa06cb9ab5b334d848e4a01360f0aaac7b38659
Rainforest <rainforestnick@gmail.com>
2020-03-28 19:13:52 +0800

    branch init

--- lib/pages/projects.dart
+++ lib/pages/projects.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_website/components/myAppBar.dart';
 import 'package:personal_website/components/settingsForm.dart';
 import 'package:personal_website/layouts/default.dart';
 import 'package:personal_website/routes/routes.dart';
@@ -10,20 +11,15 @@ class ProjectsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final routesNotifier = Provider.of<Routes>(context);
     return DefaultLayout(
-      appBar: AppBar(
+      appBar: MyAppBar(
         title: Text('Projects'),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.settings),
-            tooltip: 'Settings',
-            onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => SettingsForm()))
-          ),
-          IconButton(
-            icon: const Icon(Icons.settings_input_composite),
-            tooltip: 'kdcjks',
-            onPressed: () {
-              routesNotifier.router.navigateTo(context, 'jflsjlgjldk');
-            }
+              icon: const Icon(Icons.settings),
+              tooltip: 'Settings',
+              onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (_) => SettingsForm())
+              )
           ),
         ],
       ),
