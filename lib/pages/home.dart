c596c085b8c0d7afdf4ddce135b114357ef91f3e
Rainforest <rainforestnick@gmail.com>
2020-03-30 18:29:08 +0800

    replace customscrollview with pageview in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,163 +10,127 @@ import 'package:personal_website/responsive.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultLayout(
-        appBar: AppBar(
-          title: Text(
-            'Rainforest',
-            // style: Theme.of(context).textTheme.headline6,
+    final controller = PageController(
+      initialPage: 0
+    );
+
+    final _getProfileWidget = (MainAxisAlignment mainAxisAlignment, CrossAxisAlignment crossAxisAlignment) => Column(
+      crossAxisAlignment: crossAxisAlignment,
+      mainAxisAlignment: mainAxisAlignment,
+      children: <Widget>[
+        Text(
+          '鄭羽霖', 
+          style: Theme.of(context).textTheme.headline2.apply(
+            color: Theme.of(context).primaryColor,
+          )
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Text(
+            '研究生＠台大土木電腦輔助工程組\nSpecialize in ML/DL, Frontend',
+            style: Theme.of(context).textTheme.subtitle1.apply(
+              color: Theme.of(context).textTheme.subtitle1.color.withAlpha(150)
+            )
           ),
         ),
-        body: CustomScrollView(
-          physics: ScrollPhysics(),
-          // controller: ,
-          slivers: <Widget>[
-            SliverToBoxAdapter(
-              child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 20),
-                  child: ResponsiveHelper().isXsmall(context) ?
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Flexible(
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: ClipOval(
-                              child: Image(
-                                image: AssetImage('assets/images/profile.JPG'),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                '鄭羽霖', 
-                                style: Theme.of(context).textTheme.headline2.apply(
-                                  color: Theme.of(context).primaryColor,
-                                )
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 10),
-                                child: Text(
-                                  '研究生＠台大土木電腦輔助工程組\nSpecialize in ML/DL, Frontend',
-                                  style: Theme.of(context).textTheme.subtitle1.apply(
-                                    color: Theme.of(context).textTheme.subtitle1.color.withAlpha(150)
-                                  )
-                                ),
-                              ),
-                              BulletList(
-                                texts: [
-                                  '熱衷學習、使用各種前端框架開發',
-                                  '注重良好 UI/UX',
-                                  '質感、美感偏執，希望能參與各種酷炫視覺效果之應用開發'
-                                ],
-                                textStyle: Theme.of(context).textTheme.headline6,
-                              )
-                            ],
-                          ),
-                        )
-                      ]
+        BulletList(
+          texts: [
+            '熱衷學習、使用各種前端框架開發',
+            '注重良好 UI/UX',
+            '質感、美感偏執，希望能參與各種酷炫視覺效果之應用開發'
+          ],
+          textStyle: Theme.of(context).textTheme.headline6,
+        )
+      ],
+    );
+    final profileImage = Image(
+      image: AssetImage('assets/images/profile.JPG'),
+      fit: BoxFit.cover,
+    );
+
+    return DefaultLayout(
+      appBar: AppBar(
+        title: Text(
+          'Rainforest',
+          // style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 20),
+        child: PageView(
+          controller: controller,
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.width / 20),
+              child: ResponsiveHelper().isXsmall(context) ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      child: ClipOval(
+                        child: profileImage
+                      ),
+                    ),
+                  ),
+                  Flexible(child: _getProfileWidget(MainAxisAlignment.start, CrossAxisAlignment.start))
+                ],
+              ) : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(300),
+                      child: profileImage
+                    ),
+                  ),
+                  Flexible(
+                    child: Transform.translate(
+                      offset: Offset(-100, 0),
+                      child: _getProfileWidget(MainAxisAlignment.center, CrossAxisAlignment.start)
                     )
-                    : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Flexible(
-                          child: FractionallySizedBox(
-                            widthFactor: 1,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(300),
-                              child: Image(
-                                image: AssetImage('assets/images/profile.JPG'),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          child: Transform.translate(
-                            offset: Offset(-100, 0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  '鄭羽霖', 
-                                  style: Theme.of(context).textTheme.headline2.apply(
-                                    color: Theme.of(context).primaryColor,
-                                  )
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: 10),
-                                  child: Text(
-                                    '研究生＠台大土木電腦輔助工程組\nSpecialize in ML/DL, Frontend',
-                                    style: Theme.of(context).textTheme.subtitle1.apply(
-                                      color: Theme.of(context).textTheme.subtitle1.color.withAlpha(150)
-                                    )
-                                  ),
-                                ),
-                                BulletList(
-                                  texts: [
-                                    '熱衷學習、使用各種前端框架開發',
-                                    '注重良好 UI/UX',
-                                    '質感、美感偏執，希望能參與各種酷炫視覺效果之應用開發'
-                                  ],
-                                  textStyle: Theme.of(context).textTheme.headline6,
-                                )
-                              ],
-                            ),
-                          ),
-                        )
-                      ])),
+                  )
+                ],
+              ),
             ),
-            SliverToBoxAdapter(
-              child: Container(
-                  padding: EdgeInsets.all(MediaQuery.of(context).size.width / 20),
-                  height: MediaQuery.of(context).size.height,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Flexible(
-                        flex: 2,
-                        child: Text(
-                          '經歷', 
-                          style: Theme.of(context).textTheme.headline3
-                        )
-                      ),
-                      Flexible(child: FractionallySizedBox(heightFactor: 0.2,)),
-                      Flexible(
-                        flex: 2,
-                        child: new Timeline(
-                          events: events,
-                        ),
-                      ),
-                    ],
-                  )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  flex: 2,
+                  child: Text(
+                    '經歷', 
+                    style: Theme.of(context).textTheme.headline3
+                  )
+                ),
+                Flexible(child: FractionallySizedBox(heightFactor: 0.2,)),
+                Flexible(
+                  flex: 2,
+                  child: new Timeline(
+                    events: events,
+                  ),
+                ),
+              ],
             ),
-            SliverPadding(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.width / 20),
-              sliver: SliverGrid(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return SkillCard(skill: skills[index],);
-                  },
-                  childCount: skills.length,
-                ), 
+            Padding(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.width / 20),
+              child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: ResponsiveHelper().columns(context, 1, 1, 3, 4, 5),
                   mainAxisSpacing: 30,
                   crossAxisSpacing: 30,
-                )
+                ),
+                itemCount: skills.length,
+                itemBuilder: (_, int index) => SkillCard(skill: skills[index],),
               ),
-            )
+            ),
           ],
-        ));
+        ),
+      )
+    );
   }
 }
--- lib/pages/projects.dart
+++ lib/pages/projects.dart
@@ -3,14 +3,11 @@ import 'package:personal_website/components/projectCard.dart';
 import 'package:personal_website/layouts/default.dart';
 import 'package:personal_website/models/project.dart';
 import 'package:personal_website/responsive.dart';
-import 'package:personal_website/routes/routes.dart';
-import 'package:provider/provider.dart';
 
 class ProjectsPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    final routesNotifier = Provider.of<Routes>(context);
     return DefaultLayout(
       appBar: AppBar(
         title: Text(
@@ -18,7 +15,7 @@ class ProjectsPage extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(MediaQuery.of(context).size.width / 100),
+        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 20),
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: ResponsiveHelper().columns(context, 1, 1, 3, 4, 5),
