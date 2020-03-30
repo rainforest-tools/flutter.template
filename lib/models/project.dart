c0853a12c10d302a3e49eda11078138a59834801
Rainforest <rainforestnick@gmail.com>
2020-03-31 01:54:24 +0800

    adjust projectCard layout

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -20,6 +20,7 @@ final projects = <Project>[
     description: '整合訓練好的機器學習模型預測隨機產生的圖騰之力學強度，透過網頁讓參觀的民眾理解我們攤位具體的展覽內容。',
     url: 'https://github.com/r08521610/future_tech',
     deployUrl: 'https://future-tech-competition.herokuapp.com/',
+    imageUrl: 'assets/images/projects/未來科技展微結構設計成果展示平台.png',
     tags: [
       tags['VUE'],
       tags['NUXT'],
@@ -36,6 +37,7 @@ final projects = <Project>[
     description: 'Asian Pacific Congress on Computational Mechanics (APCOM) 會議議程表，考量與會者會有行動端的需求，特別優化 RWD 的部分。',
     url: 'https://github.com/r08521610/apcom_program',
     deployUrl: 'https://r08521610.github.io/apcom_program/#/program/18',
+    imageUrl: 'assets/images/projects/APCOM 議程網頁.png',
     tags: [
       tags['VUE'],
       tags['FRONTEND'],
@@ -50,6 +52,7 @@ final projects = <Project>[
     name: 'Palette Manager',
     description: '存放管理色票的網站。',
     url: 'https://github.com/b04501102/midterm_palette_manager',
+    imageUrl: 'assets/images/projects/Palette Manager.png',
     tags: [
       tags['REACT'],
       tags['NEXT'],
@@ -65,6 +68,7 @@ final projects = <Project>[
       new DateTime.utc(2019, 4),
     ],
     name: 'BIM 管理平台',
+    description: '整合 BIM(Building Information Modeling) 資訊及工程各階段文檔、數據，並進一步視覺化。',
     tags: [
       tags['REACT'],
       tags['FRONTEND']
@@ -75,8 +79,10 @@ final projects = <Project>[
       new DateTime.utc(2019, 7),
     ],
     name: 'Workshop Slides and Examples',
+    description: '2019 年暑假時在台大土木電腦輔助工程組暑期實習中給實習生介紹前端框架 Vue 所用簡報及範例。',
     url: 'https://github.com/b04501102/TalkDemo_20190708',
     deployUrl: 'https://talk-demo-20190708.herokuapp.com/#/',
+    imageUrl: 'assets/images/projects/Workshop Slides and Examples.png',
     tags: [
       tags['VUE'],
       tags['FRONTEND']
@@ -87,7 +93,9 @@ final projects = <Project>[
       new DateTime.utc(2019, 2),
     ],
     name: 'Cards Carousel',
+    description: '嘗試不同的 Carousel 的呈現方式。',
     url: 'https://codepen.io/rainforest80256/pen/xBwPJJ',
+    imageUrl: 'assets/images/projects/Cards Carousel.png',
     tags: [
       tags['FRONTEND']
     ]
@@ -97,8 +105,11 @@ final projects = <Project>[
       new DateTime.utc(2018, 10),
     ],
     name: 'Color Editor',
+    description: '練習 Vue 資料綁定及試做顏色選取、編輯介面。',
     url: 'https://codepen.io/rainforest80256/pen/WajJax',
+    imageUrl: 'assets/images/projects/Color Editor.png',
     tags: [
+      tags['Vue'],
       tags['FRONTEND']
     ]
   ),
@@ -107,7 +118,9 @@ final projects = <Project>[
       new DateTime.utc(2018, 8),
     ],
     name: 'TSA Handbook',
+    description: '用前端切版技術完成大學系上學術交流活動活動手冊。',
     url: 'https://codepen.io/collection/DJNGZN',
+    imageUrl: 'assets/images/projects/TSA Handbook.png',
     tags: [
       tags['FRONTEND']
     ]
@@ -119,9 +132,10 @@ class Project {
   String name;
   String description;
   String url;
+  String imageUrl;
   List<Tag> tags;
 
-  Project({this.timestamp, this.name, this.description = '', this.url, this.tags = const []});
+  Project({this.timestamp, this.name, this.description = '', this.url, this.imageUrl, this.tags = const []});
 }
 
 class FrontendProject extends Project {
@@ -132,6 +146,7 @@ class FrontendProject extends Project {
     String name, 
     String description = '', 
     String url, 
+    String imageUrl,
     this.deployUrl, 
     List<Tag> tags = const []
   }) : super(
@@ -139,6 +154,7 @@ class FrontendProject extends Project {
     name: name, 
     description: description,
     url: url, 
+    imageUrl: imageUrl,
     tags: tags
   );
 }
@@ -148,12 +164,14 @@ class CodepenProject extends Project {
     String name, 
     String description = '', 
     String url, 
+    String imageUrl,
     List<Tag> tags = const []
   }) : super(
     timestamp: timestamp, 
     name: name, 
     description: description,
     url: url, 
+    imageUrl: imageUrl,
     tags: tags
   );
 }
